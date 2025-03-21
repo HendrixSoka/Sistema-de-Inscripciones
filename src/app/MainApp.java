5db69a6945f8c96d2868d2df0a5bfdcb9c6d117f
Mauricio Teran Limari <teranmauricio22@gmail.com>
2025-03-21 16:51:07 -0400

    Arreglo FRM Documentation,Login y MainMenu

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -13,7 +13,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         try {
             // Cargar la vista desde el archivo FXML
-            Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
             
             // Crear la escena con la vista cargada
             Scene scene = new Scene(root);
