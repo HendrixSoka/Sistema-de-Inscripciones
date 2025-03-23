d770dfbab9e05e0af479a7ec1aee5eed8fcb6e0c
Hendrix <>
2025-03-23 14:38:33 -0400

    CSS y navegacion de paginas añdido(solo para la funcionalidad de Inscripciones)

--- src/controller/ExistingStudentController.java
+++ src/controller/ExistingStudentController.java
@@ -4,23 +4,41 @@
  */
 package controller;
 
+import interfaces.MainControllerAware;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
  *
  * @author intel
  */
-public class ExistingStudentController implements Initializable {
+public class ExistingStudentController implements Initializable, MainControllerAware {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnDocumentacion;
+    
+    private MainMenuController mainController;
+
+    @Override
+    public void setMainController(MainMenuController mainController) {
+        this.mainController = mainController;
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        btnDocumentacion.setOnAction(e -> navigateTo("Documentacion", "Documentation"));
     }    
-    
+    public void navigateTo(String pageName, String fxmlName){
+        System.out.println("Presionado " + pageName);
+        if (mainController != null) {
+            mainController.loadView(fxmlName);
+            mainController.addPage(pageName,fxmlName);
+        } else {
+            System.out.println("Error: MainMenuController no está disponible.");
+        }
+    }
 }
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -7,20 +7,75 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author intel
- */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       
     }    
+     @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+    
+    @FXML
+    private Button LoginButton;
+
+    //@FXML
+    //private Label messageLabel;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        System.out.println(username + " "+ password);
+        // Verificar si el usuario y la contraseña son "eu"
+        if ("eu".equals(username) && "eu".equals(password)) {
+            // cambiar vista
+            try {
+            // Cargar la nueva vista
+            Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+
+            // Obtener el stage actual desde el botón
+            Stage stage = (Stage) LoginButton.getScene().getWindow();
+
+            // Crear una nueva escena y establecerla en el stage
+            Scene scene = new Scene(root);
+            stage.setTitle("Sistema de Registro de Estudiantes");
+            stage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            
+        } else {
+            showAlert("Error", "Usuario o contraseña incorrectos", Alert.AlertType.ERROR);
+        }
+    }
+
+    private void showAlert(String title, String message, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
     
 }
